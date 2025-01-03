7a89d25ce9e9e0f9a80f3be2a598dd6a2fd699eb
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2025-01-03 11:20:15 +0500

    Update client.js

--- client/client.js
+++ client/client.js
@@ -1,20 +1,2 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const path = require('path');
-
-app.use(bodyParser.json());
-
-const PORT = 3000;
-
-
-// --- Render to /books --- 
-app.get('/', (req, res) => {
-    res.redirect("/books");
-});
-
-
-// Server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});  
+const axios = require('axios');
+const baseURL = 'http://localhost:3000';
