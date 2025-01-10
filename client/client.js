104d570ad14f3a0ca21b69e5138fe1a1c594e1a4
Mubeen-Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2025-01-10 11:17:21 +0500

    Added async function to fetch all books using Axios

--- client/client.js
+++ client/client.js
@@ -1,2 +1,14 @@
 const axios = require('axios');
 const baseURL = 'http://localhost:3000';
+
+
+// Task 10
+async function getAllBooks() {
+    try {
+      const response = await axios.get(`${baseURL}/books`);
+      console.log("All Books:", response.data);
+    } catch (err) {
+      console.error(err.message);
+    }
+}
+
